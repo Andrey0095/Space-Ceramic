8ac1ca74d373a200c09e5204c3b58863d235d638
piv2003 <piv2003@gmail.com>
2020-11-15 19:52:44 +0200

    Небольшие правки и проверка через интерфейс

--- js/script.js
+++ js/script.js
@@ -2,22 +2,21 @@ let showUkraine = document.querySelectorAll(".ukraine");
 let showEnglish = document.querySelectorAll(".english");  
 
 
-  if (showUkraine) {  
-  for (var i = 0; i < showUkraine.length; i++) {
-    buy[i].addEventListener("click", function (evt) {
-    evt.preventDefault();
-    itemAddedToBasket.classList.remove("close");
-    });
-  };
-  iconClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    itemAddedToBasket.classList.add("close");
-  });
-  window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (!itemAddedToBasket.classList.contains("close")) {
-      itemAddedToBasket.classList.add("close");
-     }
-  }
-  });
-  }
+  // if (showUkraine) {  
+  
+
+
+  // for (var i = 0; i < showUkraine.length; i++) {
+  //   showUkraine[i].addEventListener("click", function (evt) {
+  //   evt.preventDefault();
+  //   itemAddedToBasket.classList.remove("close");
+  //   });
+  // };
+  // iconClose.addEventListener("click", function (evt) {
+  //   evt.preventDefault();
+  //   itemAddedToBasket.classList.add("close");
+  // });
+  // window.addEventListener("keydown", function (evt) {
+ 
+  // });
+  // }
