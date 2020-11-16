4c0ff3e7e53624273b371c9b4b295b4da20110a0
piv2003 <piv2003@gmail.com>
2020-11-16 09:58:40 +0200

    Работа с переключателями языков JS

--- js/script.js
+++ js/script.js
@@ -1,22 +1,42 @@
 let showUkraine = document.querySelectorAll(".ukraine");
-let showEnglish = document.querySelectorAll(".english");  
+let showEnglish = document.querySelectorAll(".english");
+
+let switcherToUkraine = document.querySelector(".ukr");  
+let switcherToEnglish = document.querySelector(".eng");
 
 
-  // if (showUkraine) {  
-  
+let key = localStorage.getItem('Language');//Получить из хранилища  значение 0 или 1
 
 
-  // for (var i = 0; i < showUkraine.length; i++) {
-  //   showUkraine[i].addEventListener("click", function (evt) {
-  //   evt.preventDefault();
-  //   itemAddedToBasket.classList.remove("close");
-  //   });
-  // };
-  // iconClose.addEventListener("click", function (evt) {
-  //   evt.preventDefault();
-  //   itemAddedToBasket.classList.add("close");
-  // });
-  // window.addEventListener("keydown", function (evt) {
- 
-  // });
-  // }
+  if (key ===0) {
+    for (var i = 0; i < showUkraine.length; i++) {
+    showUkraine[i].classList.remove("no-show");
+    showEnglish[i].classList.add("no-show");
+  };
+  }
+
+if (key ===1) {
+    for (var i = 0; i < showUkraine.length; i++) {
+    showUkraine[i].classList.add("no-show");
+    showEnglish[i].classList.remove("no-show");
+  };  
+  }
+
+
+switcherToEnglish.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  for (var i = 0; i < showUkraine.length; i++) {
+    showUkraine[i].classList.add("no-show");
+    showEnglish[i].classList.remove("no-show");
+  };  
+  localStorage.setItem('Language',1);
+});
+switcherToUkraine.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  for (var i = 0; i < showUkraine.length; i++) {
+    showUkraine[i].classList.remove("no-show");
+    showEnglish[i].classList.add("no-show");
+  };
+  localStorage.setItem('Language',0);
+});
+
