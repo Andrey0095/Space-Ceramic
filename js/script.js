d4abf0c876369377483db00e66df17600127ffec
piv2003 <piv2003@gmail.com>
2020-11-16 10:00:23 +0200

    Небольшое изменение имени в JS

--- js/script.js
+++ js/script.js
@@ -5,17 +5,17 @@ let switcherToUkraine = document.querySelector(".ukr");
 let switcherToEnglish = document.querySelector(".eng");
 
 
-let key = localStorage.getItem('Language');//Получить из хранилища  значение 0 или 1
+let keyLanguage = localStorage.getItem('Language');//Получить из хранилища  значение 0 или 1
 
 
-  if (key ===0) {
+  if (keyLanguage ===0) {
     for (var i = 0; i < showUkraine.length; i++) {
     showUkraine[i].classList.remove("no-show");
     showEnglish[i].classList.add("no-show");
   };
   }
 
-if (key ===1) {
+if (keyLanguage ===1) {
     for (var i = 0; i < showUkraine.length; i++) {
     showUkraine[i].classList.add("no-show");
     showEnglish[i].classList.remove("no-show");
